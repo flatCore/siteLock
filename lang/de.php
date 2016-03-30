2c1a1ecb8772a5e285393585222f4b8fb3040380
Patrick <support@flatcore.de>
2016-03-30 21:18:56 +0200

    add language files (en/de)

---
+++ lang/de.php
@@ -0,0 +1,20 @@
+<?php
+
+$mod_lang = array();
+
+$mod_lang['nav_overview'] = 'Übersicht';
+$mod_lang['nav_overview_title'] = 'Alle Einträge auf einen Blick';
+$mod_lang['nav_prefs'] = 'Einstellungen';
+$mod_lang['nav_prefs_title'] = 'Einstellungen';
+
+$mod_lang['label_psw'] = 'Passwort';
+$mod_lang['label_psw_help_text'] = 'Nur ausfüllen, wenn das Passwort geändert werden soll.';
+$mod_lang['label_notes'] = 'Notizen';
+
+$mod_lang['label_date'] = 'Datum';
+
+$mod_lang['label_entries'] = 'Einträge';
+$mod_lang['label_logs'] = 'Protokoll';
+
+	
+?>
---
+++ lang/en.php
@@ -0,0 +1,19 @@
+<?php
+
+$mod_lang = array();
+
+$mod_lang['nav_overview'] = 'Overview';
+$mod_lang['nav_overview_title'] = 'Overview';
+$mod_lang['nav_prefs'] = 'Preferences';
+$mod_lang['nav_prefs_title'] = 'Preferences';
+
+$mod_lang['label_psw'] = 'Password';
+$mod_lang['label_psw_help_text'] = 'Edit only, if you want to change the existing Password.';
+$mod_lang['label_notes'] = 'Notes';
+
+$mod_lang['label_date'] = 'Date';
+
+$mod_lang['label_entries'] = 'Entries';
+$mod_lang['label_logs'] = 'Logfile';
+
+?>
